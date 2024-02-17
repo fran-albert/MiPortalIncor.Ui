89cdebb591c0efb8bf76ad244b0c3bf673bf0db0
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-17 00:11:43 -0300

    Add createPatient method to PatientRepository

--- src/modules/patients/domain/PatientRepository.ts
+++ src/modules/patients/domain/PatientRepository.ts
@@ -3,7 +3,10 @@ import { Patient } from "./Patient";
 export interface PatientRepository {
   getPatient: (id: number, token: string) => Promise<Patient | undefined>;
   getAll: (token: string) => Promise<Patient[]>;
-  // create: (patient: Patient) => Promise<void>;
+  createPatient: (
+    token: string,
+    newPatient: Patient
+  ) => Promise<Patient | undefined>;
   // update: (patient: Patient) => Promise<void>;
   // delete: (id: number) => Promise<void>;
 }
