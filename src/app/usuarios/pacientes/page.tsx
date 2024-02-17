efb7037e3eeec5713819d301ee1ad912b5ab3f39
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-17 00:10:31 -0300

    Remove heading from PatientsPage

--- src/app/usuarios/pacientes/page.tsx
+++ src/app/usuarios/pacientes/page.tsx
@@ -7,9 +7,7 @@ const PatientsPage = () => {
     <div className="flex flex-col md:flex-row ">
       <div className="md:w-64 w-full"></div>
       <div className="flex-grow mt-32 p-10">
-        <h1 className="text-2xl text-start font-medium mb-4">
-          Lista de Pacientes
-        </h1>
+       
         <PatientTable />
       </div>
     </div>
