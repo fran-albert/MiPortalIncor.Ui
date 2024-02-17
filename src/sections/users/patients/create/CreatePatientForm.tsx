dbd524cbe6a2fa8bfb177e9d6dff0cd55cd38cd5
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-17 00:13:38 -0300

    Update user authentication logic

--- src/sections/users/patients/create/CreatePatientForm.tsx
+++ src/sections/users/patients/create/CreatePatientForm.tsx
@@ -6,10 +6,51 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
+import { useCustomSession } from "@/context/SessionAuthProviders";
 import { goBack } from "@/lib/utils";
+import { createPatient } from "@/modules/patients/application/create/createPatient";
+import { createApiPatientRepository } from "@/modules/patients/infra/ApiPatientRepository";
+import { useSession } from "next-auth/react";
 import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { toast } from "sonner";
+
+type Inputs = {
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  photo: string;
+};
+
 function CreatePatientForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
   const [selectedState, setSelectedState] = useState("");
+
+  const { session } = useCustomSession();
+  const token = session?.accessToken;
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const patientRepository = createApiPatientRepository();
+      const createPatientFn = createPatient(patientRepository);
+      const patientCreatetionPromise = createPatientFn(token, data);
+      toast.promise(patientCreatetionPromise, {
+        loading: "Creando paciente...",
+        success: "Paciente creado con éxito!",
+        error: "Error al crear el Paciente",
+        duration: 3000,
+      });
+    } catch (error) {
+      console.error("Error al crear el paciente", error);
+    }
+  };
+
   return (
     <>
       <div className="flex items-center justify-center bg-white border shadow-2xl rounded-lg p-4 w-1/2">
@@ -21,113 +62,113 @@ function CreatePatientForm() {
           </div>
           <Separator />
           <hr />
-          <div className="flex flex-row mt-2">
-            <div className="flex-1 pr-1">
-              <div className="mb-2 block ">
-                <Label htmlFor="name">Nombre</Label>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-row mt-2">
+              <div className="flex-1 pr-1">
+                <div className="mb-2 block ">
+                  <Label htmlFor="firstName">Nombre</Label>
+                  <Input
+                    {...register("firstName", { required: true })}
+                    className="bg-gray-200 text-gray-700"
+                  />
+                </div>
+              </div>
+              <div className="flex-1 pl-1">
+                <div className="mb-2 block">
+                  <Label htmlFor="lastName">Apellido</Label>
+                  <Input
+                    {...register("lastName", { required: true })}
+                    className="bg-gray-200 text-gray-700"
+                  />
+                  <Input
+                    {...register("photo", { required: true })}
+                    className="bg-gray-200 text-gray-700"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row">
+              <div className="flex-1 pr-1">
+                <div className="mb-2 block">
+                  <Label htmlFor="phone">Teléfono</Label>
+                  <Input
+                    name="phone"
+                    className="bg-gray-200 text-gray-700"
+                    id="phone"
+                  />
+                </div>
+              </div>
+              <div className="flex-1 pl-1">
+                <div className="mb-2 block">
+                  <Label htmlFor="healthInsurance">Obra Social</Label>
+                  <HealthInsuranceSelect />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row">
+              <div className="flex-1 pr-1">
+                <div className="mb-2 block">
+                  <Label htmlFor="userName">D.N.I.</Label>
+                  <Input
+                    className="bg-gray-200 text-gray-700"
+                    id="userName"
+                    {...register("userName", { required: true })}
+                    //  defaultValue={user?.dni ? formatearDNI(user.dni) : ""}
+                  />
+                </div>
+              </div>
+              <div className="flex-1 pl-1">
+                <div className="mb-2 block">
+                  <Label htmlFor="birthdate">Fecha de Nacimiento</Label>
+                  <Input
+                    name="birthdate"
+                    id="birthdate"
+                    className="bg-gray-200 text-gray-700"
+                    //  value={user?.birthDate ? formatDate(user.birthDate) : ""}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row">
+              <div className="flex-1 pr-1">
+                <div className="mb-2 block">
+                  <Label htmlFor="state">Provincia</Label>
+                  <StateSelect
+                    selected={selectedState}
+                    onStateChange={setSelectedState}
+                  />
+                </div>
+              </div>
+              <div className="flex-1 pl-1">
+                <div className="mb-2 block">
+                  <Label htmlFor="city">Localidad</Label>
+                  <CitySelect idState={selectedState} />
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="email">Correo Electrónico</Label>
                 <Input
-                  name="name"
-                  id="name"
+                  id="email"
                   className="bg-gray-200 text-gray-700"
-                  //   value={user?.name || ""}
+                  {...register("email", { required: true })}
                 />
               </div>
             </div>
-            <div className="flex-1 pl-1">
-              <div className="mb-2 block">
-                <Label htmlFor="lastname">Apellido</Label>
-                <Input
-                  name="lastname"
-                  className="bg-gray-200 text-gray-700"
-                  id="lastname"
-                  //  value={user?.lastname || ""}
-                />
-              </div>
+            <div className="flex justify-center">
+              <Button
+                className="mt-10 m-2"
+                variant="destructive"
+                onClick={goBack}
+              >
+                Cancelar
+              </Button>
+              <Button className="mt-10 m-2" variant="teal" type="submit">
+                Agregar
+              </Button>
             </div>
-          </div>
-          <div className="flex flex-row">
-            <div className="flex-1 pr-1">
-              <div className="mb-2 block">
-                <Label htmlFor="phone">Teléfono</Label>
-                <Input
-                  name="phone"
-                  className="bg-gray-200 text-gray-700"
-                  id="phone"
-                  value={"asdasd"}
-                />
-              </div>
-            </div>
-            <div className="flex-1 pl-1">
-              <div className="mb-2 block">
-                <Label htmlFor="healthInsurance">Obra Social</Label>
-                <HealthInsuranceSelect />
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-row">
-            <div className="flex-1 pr-1">
-              <div className="mb-2 block">
-                <Label htmlFor="dni">D.N.I.</Label>
-                <Input
-                  name="dni"
-                  className="bg-gray-200 text-gray-700"
-                  id="dni"
-                  //  defaultValue={user?.dni ? formatearDNI(user.dni) : ""}
-                />
-              </div>
-            </div>
-            <div className="flex-1 pl-1">
-              <div className="mb-2 block">
-                <Label htmlFor="birthdate">Fecha de Nacimiento</Label>
-                <Input
-                  name="birthdate"
-                  id="birthdate"
-                  className="bg-gray-200 text-gray-700"
-                  //  value={user?.birthDate ? formatDate(user.birthDate) : ""}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-row">
-            <div className="flex-1 pr-1">
-              <div className="mb-2 block">
-                <Label htmlFor="state">Provincia</Label>
-                <StateSelect
-                  selected={selectedState}
-                  onStateChange={setSelectedState}
-                />
-              </div>
-            </div>
-            <div className="flex-1 pl-1">
-              <div className="mb-2 block">
-                <Label htmlFor="city">Localidad</Label>
-                <CitySelect idState={selectedState} />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="apellido">Correo Electrónico</Label>
-              <Input
-                name="email"
-                id="email"
-                className="bg-gray-200 text-gray-700"
-                //  value={user?.email || ""}
-              />
-            </div>
-          </div>
-          <div className="flex justify-center">
-            <Button
-              className="mt-10 m-2"
-              variant="destructive"
-              onClick={goBack}
-            >
-              Cancelar
-            </Button>
-            <Button className="mt-10 m-2" variant="teal">
-              Agregar
-            </Button>
-          </div>
+          </form>
         </div>
       </div>
     </>
