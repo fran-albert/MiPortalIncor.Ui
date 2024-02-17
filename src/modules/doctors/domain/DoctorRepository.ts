c19754233e26d90e11eb947b08cafc8e217a3f05
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-17 00:10:42 -0300

    Update DoctorRepository interface with new methods

--- src/modules/doctors/domain/DoctorRepository.ts
+++ src/modules/doctors/domain/DoctorRepository.ts
@@ -2,8 +2,11 @@ import { Doctor } from "./Doctor";
 
 export interface DoctorRepository {
   getDoctor: (id: number) => Promise<Doctor>;
-  getAll: () => Promise<Doctor[]>;
-  // create: (patient: Patient) => Promise<void>;
+  getAllDoctors: (token: string) => Promise<Doctor[]>;
+  createDoctor: (
+    token: string,
+    newDoctor: Doctor
+  ) => Promise<Doctor | undefined>;
   // update: (patient: Patient) => Promise<void>;
   // delete: (id: number) => Promise<void>;
 }
