3d079d15bf37262a06298b300c3ac561ce25dc75
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-17 00:11:51 -0300

    Add loading state and session token to DoctorsTable component

--- src/sections/users/doctors/table/table.tsx
+++ src/sections/users/doctors/table/table.tsx
@@ -4,34 +4,49 @@ import { useEffect, useState } from "react";
 import { Doctor } from "@/modules/doctors/domain/Doctor";
 import { createApiDoctorRepository } from "@/modules/doctors/infra/ApiDoctorRepository";
 import { getAllDoctors } from "@/modules/doctors/application/get-all/getAllDoctors";
+import Loading from "@/components/Loading/loading";
+import { useSession } from "next-auth/react";
 
 export const DoctorsTable = () => {
+  const { data: session } = useSession();
+  const token = session?.accessToken;
   const [doctors, setDoctors] = useState<Doctor[]>([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setIsLoading(true);
     const doctorRepository = createApiDoctorRepository();
     const loadAllDoctors = getAllDoctors(doctorRepository);
 
     const fetchUsers = async () => {
       try {
-        const doctorData = await loadAllDoctors();
+        const doctorData = await loadAllDoctors(token ?? "");
         setDoctors(doctorData);
       } catch (error) {
         console.log(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchUsers();
   }, []);
 
+  if (isLoading) {
+    return <Loading isLoading />;
+  }
+
   return (
-    <DataTable
-      columns={columns}
-      data={doctors}
-      searchPlaceholder="Buscar médicos..."
-      showSearch={true}
-      addLinkPath="medicos/agregar"
-      addLinkText="Agregar Médico"
-    />
+    <>
+      {" "}
+      <h1 className="text-2xl text-start font-medium mb-4">Lista de Médicos</h1>
+      <DataTable
+        columns={columns}
+        data={doctors}
+        searchPlaceholder="Buscar médicos..."
+        showSearch={true}
+        addLinkPath="medicos/agregar"
+        addLinkText="Agregar Médico"
+      />
+    </>
   );
 };
