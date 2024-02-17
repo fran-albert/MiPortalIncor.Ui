198c96345b3719f73c68e86158577bfb526be946
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-17 00:11:46 -0300

    Refactor patient repository and add createPatient method

--- src/modules/patients/infra/ApiPatientRepository.ts
+++ src/modules/patients/infra/ApiPatientRepository.ts
@@ -18,24 +18,31 @@ export function createApiPatientRepository(): PatientRepository {
   }
 
   async function getAll(token: string): Promise<Patient[]> {
-    // const response = await axiosInstance.get(`users/patients`);
-    // const patients = response.data as Patient[];
-    // return patients;
-
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND_API}Account/patients`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const response = await axiosInstance.get(`Patient/all`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     const patient = response.data as Patient[];
+    return patient;
+  }
+
+  async function createPatient(
+    token: string,
+    newPatient: Patient
+  ): Promise<Patient> {
+    const response = await axiosInstance.post("patient/create", newPatient, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const patient = response.data as Patient;
     return patient;
   }
 
   return {
     getPatient,
     getAll,
+    createPatient,
   };
 }
