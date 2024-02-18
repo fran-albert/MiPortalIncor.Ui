ac536a1ea5355f9fae0c3bbfa67aa35e5858557d
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-18 20:15:50 -0300

    Refactor doctor table component and improve code readability

--- src/sections/users/doctors/table/table.tsx
+++ src/sections/users/doctors/table/table.tsx
@@ -1,34 +1,33 @@
 import { DataTable } from "@/components/Table/dateTable";
-import { columns } from "./columns";
+import { getColumns } from "./columns";
 import { useEffect, useState } from "react";
 import { Doctor } from "@/modules/doctors/domain/Doctor";
 import { createApiDoctorRepository } from "@/modules/doctors/infra/ApiDoctorRepository";
 import { getAllDoctors } from "@/modules/doctors/application/get-all/getAllDoctors";
 import Loading from "@/components/Loading/loading";
-import { useSession } from "next-auth/react";
 
 export const DoctorsTable = () => {
-  const { data: session } = useSession();
-  const token = session?.accessToken;
   const [doctors, setDoctors] = useState<Doctor[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const doctorRepository = createApiDoctorRepository();
+  const loadAllDoctors = getAllDoctors(doctorRepository);
+
+  const fetchDoctors = async () => {
+    try {
+      setIsLoading(true);
+      const doctorData = await loadAllDoctors();
+      setDoctors(doctorData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const doctorColumns = getColumns(fetchDoctors);
+
   useEffect(() => {
-    setIsLoading(true);
-    const doctorRepository = createApiDoctorRepository();
-    const loadAllDoctors = getAllDoctors(doctorRepository);
-
-    const fetchUsers = async () => {
-      try {
-        const doctorData = await loadAllDoctors(token ?? "");
-        setDoctors(doctorData);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchUsers();
+    fetchDoctors();
   }, []);
 
   if (isLoading) {
@@ -40,10 +39,11 @@ export const DoctorsTable = () => {
       {" "}
       <h1 className="text-2xl text-start font-medium mb-4">Lista de Médicos</h1>
       <DataTable
-        columns={columns}
+        columns={doctorColumns}
         data={doctors}
         searchPlaceholder="Buscar médicos..."
         showSearch={true}
+        searchColumn="firstName"
         addLinkPath="medicos/agregar"
         addLinkText="Agregar Médico"
       />
