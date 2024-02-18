c5ac75698c608bc6e79d7b27baa14bac60f0e6c2
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-18 20:14:45 -0300

    Add MainContainer component

---
+++ src/components/mainContainer.tsx
@@ -0,0 +1,16 @@
+import SideBar from "./sideBar";
+
+interface MainContainerProps {
+  children: React.ReactNode;
+}
+
+const MainContainer = ({ children }: MainContainerProps) => {
+  return (
+    <div className="flex">
+      <SideBar />
+      <div className="flex-grow">{children}</div>
+    </div>
+  );
+};
+
+export default MainContainer;
