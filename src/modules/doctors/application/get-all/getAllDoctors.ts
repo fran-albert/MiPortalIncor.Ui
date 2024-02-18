0b066251c1befe0804b212597118774955b2ff31
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-18 20:14:57 -0300

    Refactor getAllDoctors function to remove token parameter

--- src/modules/doctors/application/get-all/getAllDoctors.ts
+++ src/modules/doctors/application/get-all/getAllDoctors.ts
@@ -2,7 +2,7 @@ import { Doctor } from "../../domain/Doctor";
 import { DoctorRepository } from "../../domain/DoctorRepository";
 
 export function getAllDoctors(doctorRepository: DoctorRepository) {
-  return async (token: string): Promise<Doctor[]> => {
-    return await doctorRepository.getAllDoctors(token);
+  return async (): Promise<Doctor[]> => {
+    return await doctorRepository.getAllDoctors();
   };
 }
