707375463173c6a8bce1abe2eb9a2f186bc61cba
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-18 20:16:16 -0300

    Add authorization token to axios requests

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getSession } from "next-auth/react";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_API,
@@ -6,5 +7,21 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const session = await getSession();
+    const token = session?.accessToken;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
