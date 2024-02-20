a1d29279d8d312d3b97bbe93cb0c8feb71ebd9c0
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-02-19 21:28:49 -0300

    Add Button component and update getColumns function

--- src/sections/users/patients/table/columns.tsx
+++ src/sections/users/patients/table/columns.tsx
@@ -8,87 +8,105 @@ import { formatDni } from "@/common/helpers/helpers";
 import { User } from "@/modules/users/domain/User";
 import { FaRegEye } from "react-icons/fa";
 import DeletePatientDialog from "../delete/DeletePatientDialog";
+import { Button } from "@/components/ui/button";
 
-export const getColumns = (fetchPatients: () => void): ColumnDef<User>[] => [
-  {
-    accessorKey: "#",
-    header: "#",
-    cell: ({ row }) => {
-      const index = row.index;
-      return <div>{index + 1}</div>;
+export const getColumns = (
+  fetchPatients: () => void,
+  roles: { isSecretary: boolean; isDoctor: boolean }
+): ColumnDef<User>[] => {
+  const columns: ColumnDef<User>[] = [
+    {
+      accessorKey: "#",
+      header: "#",
+      cell: ({ row }) => {
+        const index = row.index;
+        return <div>{index + 1}</div>;
+      },
     },
-  },
-  {
-    accessorKey: "firstName",
-    header: "Paciente",
-    cell: ({ row }) => (
-      <div className="flex items-center">
-        <Avatar>
-          <AvatarImage
-            src={
-              row.original.photo
-                ? `https://incor-ranking.s3.us-east-1.amazonaws.com/storage/avatar/${row.original.photo}`
-                : "https://incor-ranking.s3.us-east-1.amazonaws.com/storage/avatar/default.png"
-            }
-            alt="@username"
-          />
-          <AvatarFallback>
-            {`${row.original.firstName.charAt(0)}${row.original.lastName.charAt(
-              0
-            )}`}
-          </AvatarFallback>
-        </Avatar>
-        <div className="flex flex-col ml-2">
-          {" "}
-          <p className="text-sm font-medium">
-            {row.original.lastName}, {row.original.firstName}
-          </p>
-          <span
-            style={{ fontSize: "0.75rem" }}
-            className="text-teal-800 font-bold"
-          >
-            {row.original.email}
-          </span>{" "}
+    {
+      accessorKey: "firstName",
+      header: "Paciente",
+      cell: ({ row }) => (
+        <div className="flex items-center">
+          <Avatar>
+            <AvatarImage
+              src={
+                row.original.photo
+                  ? `https://incor-ranking.s3.us-east-1.amazonaws.com/storage/avatar/${row.original.photo}`
+                  : "https://incor-ranking.s3.us-east-1.amazonaws.com/storage/avatar/default.png"
+              }
+              alt="@username"
+            />
+            <AvatarFallback>
+              {`${row.original.firstName.charAt(
+                0
+              )}${row.original.lastName.charAt(0)}`}
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col ml-2">
+            {" "}
+            <p className="text-sm font-medium">
+              {row.original.lastName}, {row.original.firstName}
+            </p>
+            <span
+              style={{ fontSize: "0.75rem" }}
+              className="text-teal-800 font-bold"
+            >
+              {row.original.email}
+            </span>{" "}
+          </div>
         </div>
-      </div>
-    ),
-  },
-  {
-    accessorKey: "dni",
-    header: "DNI",
-    cell: ({ row }) => <div>{formatDni(row.original.userName)}</div>,
-  },
-  {
-    accessorKey: "phoneNumber",
-    header: "Teléfono",
-  },
-  {
-    accessorKey: "healthInsurance",
-    header: "Obra Social",
-  },
-  {
-    header: "Domicilio",
-    cell: ({ row }) => (
-      <div className="flex items-center">
-        <div className="flex flex-col ml-2">
-          {/* {row.original.city.idState}, {""} {`${row.original.city.city}`} */}
-          Santa fe , rosario
+      ),
+    },
+    {
+      accessorKey: "dni",
+      header: "DNI",
+      cell: ({ row }) => <div>{formatDni(row.original.userName)}</div>,
+    },
+    {
+      accessorKey: "phoneNumber",
+      header: "Teléfono",
+    },
+    {
+      accessorKey: "healthInsurance",
+      header: "Obra Social",
+    },
+    {
+      header: "Domicilio",
+      cell: ({ row }) => (
+        <div className="flex items-center">
+          <div className="flex flex-col ml-2">
+            {/* {row.original.city.idState}, {""} {`${row.original.city.city}`} */}
+            Santa fe , rosario
+          </div>
         </div>
-      </div>
-    ),
-  },
-  {
-    header: " ",
-    cell: ({ row }) => (
-      <div className="flex items-center justify-end">
-        <FaRegEye className="text-gray-500 cursor-pointer" size={25} />
-        <AddLabDialog idPatient={row.original.id} />
-        <EditButton id={row.original.id} path="usuarios/pacientes" />
-        <DeletePatientDialog
-          idPatient={row.original.id}
-          onPatientDeleted={fetchPatients}
-        />
-      </div>
-    ),
-  },
-];
+      ),
+    },
+    {
+      header: " ",
+      cell: ({ row }) => (
+        <div className="flex items-center justify-end">
+          {roles.isSecretary && (
+            <>
+              <AddLabDialog idPatient={row.original.id} />
+              <FaRegEye className="text-gray-500 cursor-pointer" size={25} />
+              <EditButton id={row.original.id} path="usuarios/pacientes" />
+              <DeletePatientDialog
+                idPatient={row.original.id}
+                onPatientDeleted={fetchPatients}
+              />
+            </>
+          )}
+          {roles.isDoctor && (
+            <>
+              {/* <FaRegEye className="text-gray-500 cursor-pointer" size={25} /> */}
+              <Button>Ver Paciente</Button>
+            </>
+          )}
+        </div>
+      ),
+    },
+  ];
+
+  return columns;
+};
