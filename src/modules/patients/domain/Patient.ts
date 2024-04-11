263a1a059be858b742b766bfc93e4dc9490bff11
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-04-11 01:10:28 -0300

    no message

--- src/modules/patients/domain/Patient.ts
+++ src/modules/patients/domain/Patient.ts
@@ -2,7 +2,6 @@ import { HealthPlans } from "@/modules/healthPlans/domain/HealthPlan";
 import { User } from "@/modules/users/domain/User";
 
 export interface Patient extends User {
-  userId: number;
   cuil: string;
   dni: string;
   healthPlans: {
