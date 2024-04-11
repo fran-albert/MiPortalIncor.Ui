aefd13ae85415d43916ce8c9fc62393f1888a62f
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-04-11 01:16:31 -0300

    no message

--- src/app/restablecer-contrase%C3%B1a/page.tsx
+++ src/app/restablecer-contrase%C3%B1a/page.tsx
@@ -1,9 +1,13 @@
+import Loading from "@/components/Loading/loading";
 import ResetPasswordForm from "@/sections/Auth/Reset-Password/ResetPasswordForm";
+import { Suspense } from "react";
 
 function ResetPasswordPage() {
   return (
     <>
-      <ResetPasswordForm />
+       <Suspense fallback={<Loading isLoading />}>
+        <ResetPasswordForm />
+      </Suspense>
     </>
   );
 }
