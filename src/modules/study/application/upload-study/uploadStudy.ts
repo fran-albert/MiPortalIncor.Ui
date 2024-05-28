6b79b926b21cd6f426dd92f50656074e6d14c063
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-05-28 13:47:35 -0300

    no message

--- src/modules/study/application/upload-study/uploadStudy.ts
+++ src/modules/study/application/upload-study/uploadStudy.ts
@@ -2,7 +2,7 @@ import { StudyRepository } from "../../domain/StudyRepository";
 import { Study } from "../../domain/Study";
 
 export function uploadStudy(studyRepository: StudyRepository) {
-  return async (formData: FormData) => {
+  return async (formData: FormData): Promise<Study> => {
     return await studyRepository.uploadStudy(formData);
   };
 }
